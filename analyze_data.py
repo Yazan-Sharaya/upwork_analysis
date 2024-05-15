8e2c694c4bb40a07adf76a994da077ab96ce800f
Yazan Sharaya <yazan.sharaya.yes@gmail.com>
2024-05-15 14:16:38 +0300

    Add module docstring to describe the objectives of the analysis

---
+++ analyze_data.py
@@ -0,0 +1,15 @@
+"""
+This module implements functions to analyze and visualize the data collected using the `scrape_data` module.
+
+The analysis tries to answer the following questions:
+    1. The presence of some skills relation to number of proposals.
+    2. The skills associated with the height budgets/hourly rates.
+    3. Client location influence on budget/hourly rates.
+    4. What countries have paid the most/have the highest hourly rate paid.
+    5. The most in demand skills.
+    6. How many jobs are posted on each day of the week.
+    7. Average project length/hourly rate/budget.
+    8. The most used keywords in titles and descriptions.
+    9. Relation between long descriptions (indicative of recruiting agencies) and the client's hire rate, jobs posted,
+       average hourly rate paid and total spent.
+"""
